1e0d33392a2ee7dd78ef84bb650b1f14ee22535a
davi.visintainer <davi.visintainer@placeti.com.br>
2023-01-05 09:24:30 -0300

    transformando em formulário

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,27 +1,49 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {Component, EventEmitter, forwardRef, Input, OnInit, Output} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
-  styleUrls: ['./yes-no-button-group.component.scss']
+  styleUrls: ['./yes-no-button-group.component.scss'],
+  /* SERVE PARA PROVER UM TOKEN DE ACESSO */
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    multi: true,
+    useExisting: forwardRef(() => YesNoButtonGroupComponent)   /* ESTÁ REGISTRADO, MAS NÃO TENHO A REFERÊNCIA AINDA DO YesNo */
+  }]
 })
-export class YesNoButtonGroupComponent implements OnInit {
+export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
 
   @Input() public value: string = null;
   @Input() public label = '';
   @Output() public valueChange = new EventEmitter<string>();
   public options = YesNoButtonGroupOptions;
+  public onChange = (value: string) => {};
+  public onTouched = () => { };
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public activate(value: string): void {
+  public writeValue(value: string): void {
     this.value = value;
+    this.onChange(this.value);
     this.valueChange.emit(this.value);
   }
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
 
+  public setDisabledState(isDisabled: boolean): void {
+  }
+
+  public activate(value: string): void {
+    this.writeValue(value);
+  }
 }
 
 enum YesNoButtonGroupOptions {
